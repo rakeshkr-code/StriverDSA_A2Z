af38f2c545826451007f5e38f58d763e6352b6b2
Rakesh <mandalrakesh628@gmail.com>
2025-04-23 09:22:41 +0530

    added rest of the patterns in pattern set 1

--- StarPattern/pattern_set_1.py
+++ StarPattern/pattern_set_1.py
@@ -42,3 +42,64 @@ for i in range(n):
     for j in range(i+1):
         print("*", end=" ")
     print()
+
+# --------------------------------------------------------------
+
+# ## Pattern - 4:
+# 1
+# 1 2
+# 1 2 3
+# 1 2 3 4
+# 1 2 3 4 5
+
+print("## Pattern - 4:")
+for i in range(n):
+    for j in range(i+1):
+        print(j+1, end=" ")
+    print()
+
+# --------------------------------------------------------------
+
+# ## Pattern - 5:
+# 1
+# 2 2
+# 3 3 3
+# 4 4 4 4
+# 5 5 5 5 5
+
+print("## Pattern - 5:")
+for i in range(n):
+    for j in range(i+1):
+        print(i+1, end=" ")
+    print()
+
+# --------------------------------------------------------------
+
+# ## Pattern - 6:
+# * * * * *
+# * * * * 
+# * * * 
+# * *
+# *
+
+print("## Pattern - 6:")
+for i in range(n):
+    for j in range(n-i):
+        print("*", end=" ")
+    print()
+
+# --------------------------------------------------------------
+
+# ## Pattern - 7:
+# 1 2 3 4 5
+# 1 2 3 4
+# 1 2 3
+# 1 2
+# 1
+
+print("## Pattern - 7:")
+for i in range(n):
+    for j in range(n-i):
+        print(j+1, end=" ")
+    print()
+
